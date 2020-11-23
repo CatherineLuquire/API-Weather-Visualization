1907757eeb5834c5465059a79346be09defb91f2
CatherineLuquire <cluquire@gmail.com>
2020-11-22 23:35:00 -0500

    more editing

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "be40c66acfa4b00666e9451250d19502"
+weather_api_key = "enter your API key here"
 
 # Google API Key
-g_key = "AIzaSyChkWOYPR8-n9sLkgeAphH2_NLgWstR1bk"
+g_key = "enter your API key here"
